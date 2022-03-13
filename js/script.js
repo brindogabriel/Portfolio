15650d26f51dd110e44aa062b7991c0629612fcb
Gabriel Brindo <gabrielbrindo18@gmail.com>
2022-03-13 15:00:34 -0300

    agregar scrollspy a la nav

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// ACTIVAR TOOLTIP EN TODOS LADOS
+//* ACTIVAR TOOLTIP EN TODOS LADOS
 var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="tooltip"]')
 );
@@ -6,3 +6,7 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
 });
 
+//* SCROLLSPY
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar-example'
+})
