d53e2d159180abac004edb85c2e748a95f9816c9
Gabriel Brindo <gabrielbrindo18@gmail.com>
2023-11-12 15:42:39 -0300

    asd

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 //* ACTIVAR TOOLTIP EN TODOS LADOS
 var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
 );
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
+    return new bootstrap.Tooltip(tooltipTriggerEl);
 });
 
 //* SCROLLSPY
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-  target: '#navbar-example'
+    target: '#navbar-example'
 })
+
