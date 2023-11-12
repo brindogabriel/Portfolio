9cb13d3ea5e062bd7b75b1a934a5dbe7947e758c
Gabriel Brindo <gabrielbrindo18@gmail.com>
2023-11-12 16:02:21 -0300

    asd

--- js/script.js
+++ js/script.js
@@ -11,3 +11,14 @@ var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#navbar-example'
 })
 
+
+window.onscroll = function () {
+    var botonIrArriba = document.getElementById("botonIrArriba");
+
+    // Verifica si el desplazamiento vertical es mayor que 0
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        botonIrArriba.style.display = "block"; // Muestra el botón
+    } else {
+        botonIrArriba.style.display = "none"; // Oculta el botón
+    }
+};
