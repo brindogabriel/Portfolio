ac13b391dca3f9e16a471c144492b22636cc873c
Gabriel Brindo <35706106+brindogabriel@users.noreply.github.com>
2025-06-02 03:07:49 -0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://gabrielbrindo.com.ar',
+     url: 'https://www.gabrielbrindo.com.ar',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
